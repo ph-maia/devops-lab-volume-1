aac86600baea702ed437eaf397a7f3d4d39d26ae
Pedro Henrique Moreira da Maia <83989611+ph-maia@users.noreply.github.com>
2022-06-13 20:20:07 -0300

    ajuste msg

--- web/test/app.test.js
+++ web/test/app.test.js
@@ -34,7 +34,7 @@ describe('App Integration Test', () => {
         // Assert
         assert.equal(response.status, 200);
         assert.ok(response.text.includes('color: grey'));
-        assert.ok(response.text.includes('<h1>Aplicação - DevOps Mercurianos!</h1>'));
+        assert.ok(response.text.includes('<h1>Aplicação - DevOps Marcianos!</h1>'));
     });
 
     it('Should return npm a page with an error in title message', async () => {
