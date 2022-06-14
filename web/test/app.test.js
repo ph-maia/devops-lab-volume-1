2e7c2b7b686a003fdc0830ee8c832da481ae8187
Pedro Henrique Moreira da Maia <83989611+ph-maia@users.noreply.github.com>
2022-06-13 22:41:40 -0300

    upd tag2

--- web/test/app.test.js
+++ web/test/app.test.js
@@ -34,7 +34,7 @@ describe('App Integration Test', () => {
         // Assert
         assert.equal(response.status, 200);
         assert.ok(response.text.includes('color: blue'));
-        // assert.ok(response.text.includes('<h1>Aplicação - DevOps </h1> <a href="https://www.youtube.com/watch?v=MTbF9ALViJw&ab_channel=LucasFernando">Marcianos!</a>'));
+        // assert.ok(response.text.includes('<h1>Aplicação - DevOps <a href="https://www.youtube.com/watch?v=MTbF9ALViJw&ab_channel=LucasFernando">Marcianos!</a> </h1>'));
     });
 
     it('Should return npm a page with an error in title message', async () => {
